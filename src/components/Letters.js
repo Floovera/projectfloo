a34461daf3645c0ae0d0cc8f7dcd5571326762cc
Floovera <r0385588@student.thomasmore.be>
2022-05-28 10:27:38 +0200

    onClick toegevoegd op letters & daar gelogd in de console

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -4,10 +4,10 @@ import {Col} from "react-bootstrap";
 import {MyCard} from "./MyCard";
 
 function Letter(props) {
-    const {letter} = props;
+    const {letter,onClick} = props;
     return (
         <Col xs={6} sm={4} md={3} lg={2}>
-            <MyCard onClick={() => console.log(letter.description)} title={letter.description}>
+            <MyCard onClick={() => onClick()} title={letter.description}>
             </MyCard>
         </Col>
     );
@@ -23,7 +23,12 @@ export function Letters(props) {
     const {letters, title} = props;
     return (
         <Section title={title}>
-            {letters.map(l => <Letter key={l.id} letter={l}/>)}
+            {letters.map(l => <Letter
+                key={l.id}
+                letter={l}
+                onClick={() => console.log(l.description)}
+
+            />)}
         </Section>
     );
 }
